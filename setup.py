8c0e1285a915c5f11ac8854aac7625df276f5da0
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2017-06-07 17:10:59 +0000

    Add pypi deployment integration

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as requirements:
+    setup(
+        name='scrapy-sqs-exporter',
+        version='1.0.0',
+        py_modules=['sqsfeedexport'],
+        install_requires=requirements.read().splitlines(),
+        url='https://github.com/multiplechoice/scrapy-sqs-exporter',
+        license='MIT',
+        author='aodj',
+        author_email='alexodonovanjones@gmail.com',
+        description='Scrapy extension for outputting scraped items to an Amazon SQS instance',
+        long_description=open('README.rst').read(),
+        keywords='scrapy sqs'
+    )
